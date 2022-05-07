ee1fad29fd50f76455c539d7bd62b39da8f3a806
victoria-nsl <vbnaish@yandex.ru>
2022-05-07 18:06:40 +0300

    Рефакторинг кода с закладками

--- source/js/modules/bookmark.js
+++ source/js/modules/bookmark.js
@@ -1,100 +1,100 @@
 const buttonsСhangeBookmarks = document.querySelectorAll('.products__button-bookmark');
 
-//const listBookmarks =document.createElement('div');
-//listBookmarks.classList.add('bookmark__list');
-let arrayBookmarks = [];
+if (buttonsСhangeBookmarks) {
+  const linkBookmarks = document.querySelector('.page-header__link-navigation-user--bookmark');
+  const numberProductsInBookmark = linkBookmarks.querySelector('.page-header__link-navigation-user--bookmark span:last-child');
 
-const linkBookmarks = document.querySelector('.page-header__link-navigation-user--bookmark');
-const numberProductsInBookmark = linkBookmarks.querySelector('.page-header__link-navigation-user--bookmark span:last-child');
+  let arrayBookmarks = [];
+  /*=============LOCALSTORAGE===========*/
+  let isStorageSupport = true;
+  let storageNumberBookmarks = '';
+  let storageArrayBookmarks = '';
 
-/*=============LOCALSTORAGE===========*/
-let isStorageSupport = true;
-let storageNumberBookmarks = '';
-let storageArrayBookmarks = '';
-
-try {
-  storageNumberBookmarks = localStorage.getItem('bookmarks');
-  storageArrayBookmarks = JSON.parse(localStorage.getItem('array'));
-} catch (err) {
-  isStorageSupport = false;
-}
-
-if (storageNumberBookmarks || storageArrayBookmarks) {
-  numberProductsInBookmark.textContent = storageNumberBookmarks || '';
-  if (+numberProductsInBookmark.textContent > 0) {
-    linkBookmarks.classList.add('page-header__link-navigation-user--active');
+  //получить значения из localStorage
+  try {
+    storageNumberBookmarks = localStorage.getItem('bookmarks');
+    storageArrayBookmarks = JSON.parse(localStorage.getItem('array'));
+  } catch (err) {
+    isStorageSupport = false;
   }
 
-  arrayBookmarks = storageArrayBookmarks || '';
-  buttonsСhangeBookmarks.forEach((buttonСhangeBookmarks) => {
-    const item = buttonСhangeBookmarks.closest('.products__item');
-    if (storageArrayBookmarks.includes(item.id)) {
-      buttonСhangeBookmarks.classList.add('products__button-bookmark--active');
+  if (storageNumberBookmarks || storageArrayBookmarks) {
+    numberProductsInBookmark.textContent = storageNumberBookmarks || '';
+    if (+numberProductsInBookmark.textContent > 0) {
+      linkBookmarks.classList.add('page-header__link-navigation-user--active');
     }
-  });
-}
 
-const changeBookmarks = (button) => {
-  const product = button.closest('.products__item');
-  const idProduct = product.id;
-  let numberBookmarks = + numberProductsInBookmark.textContent;
+    arrayBookmarks = storageArrayBookmarks || '';
+    buttonsСhangeBookmarks.forEach((buttonСhangeBookmarks) => {
+      const item = buttonСhangeBookmarks.closest('.products__item');
+      if (storageArrayBookmarks.includes(item.id)) {
+        buttonСhangeBookmarks.classList.add('products__button-bookmark--active');
+      }
+    });
+  }
 
-  if(arrayBookmarks.length === 0) {
+  //установить значения в localStorage
+  const  setItemsLocalStorage  = (array, numberBookmark) => {
+    if(isStorageSupport) {
+      localStorage.setItem('bookmarks', numberBookmark.textContent);
+      localStorage.setItem('array', JSON.stringify(array));
+    }
+  };
+
+  /*=============ДОБАВЛЕНИЕ ТОВАРА В ЗАКЛАДКИ===========*/
+  const addToBookmarks = (idProduct, button, numberBookmarks) => {
     arrayBookmarks.push(idProduct);
 
     linkBookmarks.classList.add('page-header__link-navigation-user--active');
+
     button.classList.add('products__button-bookmark--active');
     button.textContent = 'В закладках';
     numberBookmarks += 1;
     numberProductsInBookmark.textContent = numberBookmarks;
 
-    if(isStorageSupport) {
-      localStorage.setItem('bookmarks', numberProductsInBookmark.textContent);
-      localStorage.setItem('array', JSON.stringify(arrayBookmarks));
+    setItemsLocalStorage (arrayBookmarks, numberProductsInBookmark);
+  };
+
+  /*=============УДАЛЕНИЕ ТОВАРА В ЗАКЛАДКИ===========*/
+  const removeFromBookmarks = (idProduct, button, numberBookmarks) => {
+    const idIndex = arrayBookmarks.indexOf(idProduct);
+    if (idIndex !== -1) {
+      arrayBookmarks.splice(idIndex, 1);
     }
-  } else {
-    if (arrayBookmarks.includes(idProduct)) {
-      const idIndex = arrayBookmarks.indexOf(idProduct);
-      if (idIndex !== -1) {
-        arrayBookmarks.splice(idIndex, 1);
-      }
 
-      button.classList.remove('products__button-bookmark--active');
-      button.textContent = 'В закладки';
-      numberBookmarks -= 1;
-      numberProductsInBookmark.textContent = numberBookmarks;
-
-      if(isStorageSupport) {
-        localStorage.setItem('bookmarks', numberProductsInBookmark.textContent);
-        localStorage.setItem('array', JSON.stringify(arrayBookmarks));
-      }
-
-      if(arrayBookmarks.length === 0) {
-        linkBookmarks.classList.remove('page-header__link-navigation-user--active');
-      }
-
-    } else {
-      arrayBookmarks.push(idProduct);
-      linkBookmarks.classList.add('page-header__link-navigation-user--active');
-      button.classList.add('products__button-bookmark--active');
-      button.textContent = 'В закладках';
-      numberBookmarks += 1;
-      numberProductsInBookmark.textContent = numberBookmarks;
-
-      if(isStorageSupport) {
-        localStorage.setItem('bookmarks', numberProductsInBookmark.textContent);
-        localStorage.setItem('array', JSON.stringify(arrayBookmarks));
-      }
+    if(arrayBookmarks.length === 0) {
+      linkBookmarks.classList.remove('page-header__link-navigation-user--active');
     }
-  }
-};
 
-//Обработчики
-const onButtonСhangeBookmarksClick = (evt) => {
-  changeBookmarks(evt.target);
-};
+    button.classList.remove('products__button-bookmark--active');
+    button.textContent = 'В закладки';
+    numberBookmarks -= 1;
+    numberProductsInBookmark.textContent = numberBookmarks;
 
-buttonsСhangeBookmarks.forEach((buttonСhangeBookmarks) => {
-  buttonСhangeBookmarks.disabled = false;
-  buttonСhangeBookmarks.addEventListener('click', onButtonСhangeBookmarksClick);
-});
+    setItemsLocalStorage (arrayBookmarks, numberProductsInBookmark);
+  };
+
+  /*=============ИЗМЕНИТЬ КОЛИЧЕСТВО ЗАКЛАДОК===========*/
+  const changeBookmarks = (buttonSelected) => {
+    const productSelected = buttonSelected.closest('.products__item');
+    const idProductSelected = productSelected.id;
+    const numberBookmarksInitial = + numberProductsInBookmark.textContent;
+
+    if(arrayBookmarks.length === 0) {
+      addToBookmarks(idProductSelected, buttonSelected, numberBookmarksInitial );
+      return;
+    }
+
+    arrayBookmarks.includes(idProductSelected) ?  removeFromBookmarks(idProductSelected, buttonSelected, numberBookmarksInitial) : addToBookmarks(idProductSelected, buttonSelected, numberBookmarksInitial );
+  };
+
+  //Обработчики
+  const onButtonСhangeBookmarksClick = (evt) => {
+    changeBookmarks(evt.target);
+  };
+
+  buttonsСhangeBookmarks.forEach((buttonСhangeBookmarks) => {
+    buttonСhangeBookmarks.disabled = false;
+    buttonСhangeBookmarks.addEventListener('click', onButtonСhangeBookmarksClick);
+  });
+}
